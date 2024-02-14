1e2c9cc3969f7008b9fc2b9dea3ac0c2adc39680
99Uz <krittin2557kub@gmail.com>
2024-02-14 13:38:32 +0700

    LabSheet13

---
+++ LabSheet13/src/SaveandOpen.java
@@ -0,0 +1,56 @@
+import java.util.*;
+import java.io.*;
+public class SaveandOpen extends Employee{
+	private String name;
+	private String dept;
+	
+	public void insert() throws IOException{
+		// create object for input data from keyboard(console)
+		Scanner scan = new Scanner(System.in);
+		// create object for write(save) data to File using PrintStream Class
+		PrintStream writeFile = new PrintStream(new File("D://txtFile//employee.txt"));
+		String answer;
+		do {
+			header(); //call header() method from Class Employee
+			System.out.print("Enter name	: ");
+			name = scan.next();
+			System.out.print("Enter department : ");
+			dept = scan.next();
+			//save all data to file
+			writeFile.println(name + "\t" + dept);
+			System.out.print("Enter data again? : ");
+			answer = scan.next().toLowerCase();
+		}while(answer.equals("y"));
+
+	}
+	
+	public void read() {
+		try {
+			//create object for read File using Scanner Class
+			Scanner readFile = new Scanner(new File("D://txtFile//employee.txt"));
+			boolean check = false;
+			int i = 1;
+			header();
+			while(readFile.hasNext()) {
+				name = readFile.next();
+				dept = readFile.next();
+				if(dept.equalsIgnoreCase(super.getDept())) {
+					System.out.println(i + ") " + name);
+					check = true;
+					i++;
+				}
+			}
+			if(check == true) {
+				header();
+				System.out.println("Employee in department " + super.getDept() + " is " + (i-1) + " person.");
+			}
+			if(check == false) {
+				System.out.println("\nSorry, no department : " + super.getDept() + " in File");
+			}
+			readFile.close();
+		}
+		catch(IOException e) {
+			System.out.println("\nSorry, file not found...");
+		}
+	}
+}
